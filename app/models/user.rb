3fe4e82574b2f91059954e78126ddb08ab49d0ed
Rey David Cuevas <cuevas.vela@gmail.com>
2020-04-10 12:33:50 -0500

    added user#create on the controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 200 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  post 'registration', to: 'users#create'
+  get 'registration', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
 end
