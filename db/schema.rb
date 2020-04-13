b277451b8706556c1768c638eda7f073105628cf
Rey David Cuevas <cuevas.vela@gmail.com>
2020-04-13 16:27:52 -0500

    created controller and model record

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_225645) do
+ActiveRecord::Schema.define(version: 2020_04_13_212526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2020_04_10_225645) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.integer "authorid"
+    t.string "name"
+    t.integer "minutes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
