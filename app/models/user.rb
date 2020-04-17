740389fbeaa31e7c97e202ace69d684541cfae1c
Rey David Cuevas <cuevas.vela@gmail.com>
2020-04-17 13:06:11 -0500

    update double validation (email is invalid and email is blank) to only email invalid

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100 }
   validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
-  validates :email, presence: true, length: { maximum: 200 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :email, length: { maximum: 200 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   has_many :groups
   has_many :records, foreign_key: 'authorid'
 
