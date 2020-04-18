05557e7367abc23cd1cb88ac00b777e586c29d96
Rey David Cuevas <cuevas.vela@gmail.com>
2020-04-17 23:26:25 -0500

    added more test for rspec

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+RSpec.describe Group, type: :model do
+  let(:user1) do
+    User.create(name: 'example_user', username: 'example', email: 'user@email.com')
+  end
+
+  let(:record1) do
+    Record.new(name: 'second', minutes: 45, author: user1)
+  end
+
+  subject do
+    described_class.new(name: 'example', user: user1)
+  end
+
+  it 'a group is saved' do
+    expect(Group.all.count).to eq(0)
+    subject.save(validate: false)
+    expect(Group.all.count).to eq(1)
+  end
+
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a user' do
+    subject.user = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without an icon' do
+    expect(subject).to_not be_valid
+  end
+
+  it 'shows the number of the record to the group' do
+    subject.save(validate: false)
+    record1.group = subject
+    record1.save
+    expect(subject.records.count).to eq(1)
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-ENV["RAILS_ENV"] ||= "test"
-require File.expand_path("../../config/environment", __FILE__)
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require "rspec/rails"
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
